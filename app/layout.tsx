f477d23e08537e622473e803ef68c7ef9389e9f3
fadelanumah <fadelanumah@gmail.com>
2026-03-27 08:43:51 +0700

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +29,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <GoogleAnalytics gaId='G-1VCH9H6NML' />
     </html>
   );
 }
