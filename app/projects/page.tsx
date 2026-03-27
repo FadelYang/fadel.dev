1c074fd7b00863189fb1ce1eb6ee9ee0ae4a0506
fadelanumah <fadelanumah@gmail.com>
2026-03-27 14:27:03 +0700

    feat(projetcts): add pages

---
+++ app/projects/page.tsx
@@ -0,0 +1,17 @@
+import { Suspense } from 'react';
+import Header from '../(homepage)/header';
+import { getAllPosts } from '@/lib/blog';
+import ProjectPageClient from './page-hero';
+
+export default function Blogs() {
+  const posts = getAllPosts();
+
+  return (
+    <div>
+      <Suspense>
+        <Header />
+      </Suspense>
+      <ProjectPageClient posts={posts} />
+    </div>
+  );
+}
